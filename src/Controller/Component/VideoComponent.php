06c5a65fdbe03684f79bbade02f9a2851a73b190
Kuldeep Joshi <Kuldeep.joshi197@gmail.com>
2020-08-23 14:03:10 +0100

    Controller and View
    Created VideoController and VideoSearch view.
    Template for VideoSearchview is Vidoedisplay.php

---
+++ src/Controller/Component/VideoComponent.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of VideoComponent
+ *
+ * @author kuldeep joshi
+ * created 23/08/2020
+ */
+class VideoComponent  extends Component {
+    //put your code here
+}
